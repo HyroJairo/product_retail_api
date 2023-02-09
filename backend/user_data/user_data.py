17500bc0e356c35eb3cc2e17bfc5703277d1fa1d
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-02-09 13:36:21 -0800

    Moved to backend folder away from parent level

---
+++ backend/user_data/user_data.py
@@ -0,0 +1,31 @@
+import sqlalchemy
+from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy.orm import declarative_base
+
+sqlite_connect = "sqlite+pysqlite:///user_data/database/userData.db"
+
+engine = sqlalchemy.create_engine(sqlite_connect)
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__= "users"
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_name = Column(String, unique=False, nullable=False)
+    password = Column(String(25), unique=False, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    address = Column(String, unique=False, nullable=False)
+    payment_methods = Column(String, unique=False, nullable=False)
+
+
+class Products(Base):
+    __tablename__="products"
+    product_id = Column(Integer, primary_key=True, autoincrement=True)
+    product_name = Column(String)
+    product_price = Column(Float)
+    product_category = Column(String)
+    product_description = Column(String)
+
+
+
+Base.metadata.create_all(bind=engine)
