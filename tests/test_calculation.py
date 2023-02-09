dd7e99da73da86484a33c9750e96eafcb4420ec4
mustafaradheyyan <31796217+mustafaradheyyan@users.noreply.github.com>
2023-02-09 10:31:47 -0800

    Update test_calculation.py

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,6 +1,6 @@
 import sys
-#sys.path.insert(0, "calculation")
-sys.path.insert(0, "../calculation/")
+sys.path.insert(0, "calculation")
+#sys.path.insert(0, "../calculation/")
 from calculation import addition
 
 def test_addition():
