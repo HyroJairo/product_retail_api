72c9d104347b39bef200e53aa6995bee7b72e184
asmith283 <asmith41234@gmail.com>
2023-02-09 21:08:59 -0800

    test

--- ikeaAPI.py
+++ ikeaAPI.py
@@ -1,8 +1,10 @@
+import os
 from flask import request, Flask
 from user_data import Users
 import sqlalchemy
 from sqlalchemy import select
 import pandas as pd
+#import products.main as prd
 app = Flask(__name__)
 
 logged_in = False
@@ -10,34 +12,54 @@ logged_in = False
 
 @app.route('/register', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def register():
-    sqlite_connect = "sqlite+pysqlite:///groupProject4/product_retail_api/userData.db"
+    sqlite_path = "sqlite+pysqlite:///groupProject4/product_retail_api/backend/user_data/database/userData.db"
+    if os.path.exists(sqlite_path):
+        sqlite_connect = "sqlite+pysqlite:///groupProject4/product_retail_api/backend/user_data/database/userData.db"
+    else:
+        sqlite_connect = "sqlite+pysqlite:///user_data/database/userData.db"
+        
     engine = sqlalchemy.create_engine(sqlite_connect)
     if request.method=='POST':
-        un = input("please enter your name: ")
-        pw = input("please enter a password: ")
-        em = input("please enter an email: ")
-        ad = input("please enter an address: ")
-        pm = input("please enter your prefered payment methods: ")
+        un = request.json["name"]
+        pw = request.json["password"]
+        em = request.json["email"]
+        ad = request.json["address"]
+        pm = request.json["payment_method"]
+        # un = input("please enter your name: ")
+        # pw = input("please enter a password: ")
+        # em = input("please enter an email: ")
+        # ad = input("please enter an address: ")
+        # pm = input("please enter your prefered payment methods: ")
         #confirm_password=input("")
         with engine.connect() as conn:
-            stmt = sqlalchemy.insert(Users).values(user_name=un, password=pw, email=em, address=ad, payment_methods=pm)
-            conn.execute(stmt)
+            try:
+                stmt = sqlalchemy.insert(Users).values(user_name=un, password=pw, email=em, address=ad, payment_methods=pm)
+                conn.execute(stmt)
+            except Exception as e:
+                return "email already exists"
             conn.commit()
         return 'account added'
     elif request.method=='PUT':
+        current_email = request.json["current_email"]
+        un = request.json["name"]
+        pw = request.json["password"]
+        em = request.json["email"]
+        ad = request.json["address"]
+        pm = request.json["payment_method"]
         with engine.connect() as conn:
-            current_email=input("What is your email: ")
-            un = input("please enter your name: ")
-            pw = input("please enter a password: ")
-            em = input("please enter an email: ")
-            ad = input("please enter an address: ")
-            pm = input("please enter your prefered payment methods: ")
+            # current_email=input("What is your email: ")
+            # un = input("please enter your name: ")
+            # pw = input("please enter a password: ")
+            # em = input("please enter an email: ")
+            # ad = input("please enter an address: ")
+            # pm = input("please enter your prefered payment methods: ")
             stmt = sqlalchemy.update(Users).where(Users.email==current_email).values(user_name=un, password=pw, email=em, address=ad, payment_methods=pm)
             conn.execute(stmt)
             conn.commit()
         return 'account updated'
     elif request.method=='DELETE':
-        email=input("What is your email: ")
+        email = request.json["email"]
+        #email=input("What is your email: ")
         stmt = sqlalchemy.delete(Users).where(Users.email==email)
         with engine.connect() as conn:
             conn.execute(stmt)
@@ -45,16 +67,26 @@ def register():
         return 'account deleted'
     return 'nothing here'
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['POST'])
 def login():
+    print("reached login")
+    
     global logged_in
-    sqlite_connect = "sqlite+pysqlite:///groupProject4/product_retail_api/userData.db"
+    sqlite_path = "sqlite+pysqlite:///groupProject4/product_retail_api/backend/user_data/database/userData.db"
+    if os.path.exists(sqlite_path):
+        sqlite_connect = "sqlite+pysqlite:///groupProject4/product_retail_api/backend/user_data/database/userData.db"
+    else:
+        sqlite_connect = "sqlite+pysqlite:///user_data/database/userData.db"
     engine = sqlalchemy.create_engine(sqlite_connect)
     
-    username= input("Enter your name and password ")
-    password = input("Enter your password ")
+    email = request.json["email"]
+    password = request.json["password"]
+
+    
+    # username= input("Enter your name and password ")
+    # password = input("Enter your password ")
     with engine.connect() as conn:
-        db = pd.read_sql(select(Users.user_name==username), conn)
+        db = pd.read_sql(select(Users.email==email), conn)
         dp = pd.read_sql(select(Users.password==password), conn)
         newdb = db.values.tolist() 
         newdp = dp.values.tolist()
@@ -65,14 +97,23 @@ def login():
                 logged_in = True
                 return 'logged in'
         return 'invalid'
-                
+            
     # if session.query(Users).filter(Users.user_name==username) & session.query(Users).filter(Users.password==password):
     #     logged_in = True
     #     return 'Logged In'
     # else:
     #     return 'invalid name or password'
         
+@app.route('/logout', methods=['POST'])
+def logout():
+    global logged_in
+    if logged_in == True:
+        logged_in = False
+        return 'Logged Out'
+    else:
+        return 'logged in already'
 
 
 if __name__ =='__main__':
+    #prd.main()
     app.run(debug=True) #local host 5000
