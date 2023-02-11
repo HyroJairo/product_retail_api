dd38f13093d8e61b487fcfdad94387448a8e4738
Payton Loppnow <pmloppnow@usf.edu>
2023-02-10 22:42:31 -0500

    created product_reviews and finished search funcs

--- backend/products/main.py
+++ backend/products/main.py
@@ -1,9 +1,10 @@
 import products.ikea_products.ikea_products as ikp
+import products.product_reviews.product_reviews as pr
 import products.database.curd as dbc
 import products.search as dbs
 
 def get_user_input() -> int:
-    RETRY_STRING = "That is not a valid choice! (Must be an integer from 0-4)"
+    RETRY_STRING = "That is not a valid choice! (Must be an integer from 0-8)"
     
     while(True):
         try:
@@ -11,14 +12,15 @@ def get_user_input() -> int:
                             What do you want to do? (type 0-4, followed by enter)\n
                             0) Proceed to server
                             
-         Ikea Products Database 1) CREATE         2) UPDATE
-                                3) READ           4) DELETE
-                                5) SORT BY PRICE  6) SORT BY CATEGORY\n
+         Ikea Products Database 1) CREATE              2) UPDATE
+                                3) READ                4) DELETE
+                                5) SORT BY PRICE       6) SORT BY CATEGORY
+                                7) SORT BY POPULARITY  8) CUSTOM QUERY\n
                             """))
         except ValueError:
             print(RETRY_STRING)
         else:
-            if user_choice >= 0 and user_choice < 7: 
+            if user_choice >= 0 and user_choice < 9: 
                 break
             else:
                 print(RETRY_STRING)
@@ -27,7 +29,10 @@ def get_user_input() -> int:
 def main():
     dbc.open_connection()
     ikea_products = ikp.IkeaProducts()
+    product_reviews = pr.ProductReviews()
+    product_reviews.gen_product_reviews_df(10)
     dbc.persist_dataset("products", ikea_products.ikea_products_df)
+    dbc.persist_dataset("reviews", product_reviews.product_reviews_df)
     
     while(True):
         user_choice = get_user_input()
@@ -47,6 +52,10 @@ def main():
             dbs.sort_by_price()
         elif user_choice == 6:
             dbs.sort_by_category()
+        elif user_choice == 7:
+            dbs.sort_by_popularity()
+        elif user_choice == 8:
+            dbs.custom_query(ikea_products.ikea_products_columns_list)
 
 if __name__ == "__main__":
     main()
