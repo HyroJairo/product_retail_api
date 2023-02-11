419645ee15782f33a84c7ea1b0dfc1f5847d9c69
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-02-11 12:49:27 -0800

    Removed sample test scripts

--- calculation/calculation.py
+++
@@ -1,2 +0,0 @@
-def addition(arg1, arg2):
-  return arg1 + arg2
--- tests/test_calculation.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, "calculation")
-from calculation import addition
-
-def test_addition():
-    arg1, arg2 = 3, 10
-    assert(addition(arg1, arg2) == 3 + 10)
