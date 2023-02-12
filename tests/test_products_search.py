5e9d3ef656d621d40209649d289cf9f5aaaf070d
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-02-11 23:42:01 -0800

    Refactored search products test module

--- tests/test_products_search.py
+++ tests/test_products_search.py
@@ -10,6 +10,12 @@ import database_schemas
 
 DATABASE_NAME = "test.db"
     
+def pre_setup(schema, add_products):
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.execute(schema)
+    add_products(conn)
+    return conn
+    
 def post_setup(conn):
     conn.close()
     os.remove(DATABASE_NAME)
@@ -19,12 +25,6 @@ class TestProduct():
     PRIMARY_KEY = "product_id"
     ATTRIBUTES = ["name", "price", "category", "description"]
     
-    def pre_setup(self):
-        conn = sqlite3.connect(DATABASE_NAME)
-        conn.execute(database_schemas.create_products)
-        self.add_products_to_table(conn)
-        return conn
-    
     def add_products_to_table(self, conn):
         curd.add_data(TestProduct.TABLE_NAME, TestProduct.PRIMARY_KEY, TestProduct.ATTRIBUTES, ["Vardar", 11.55, "Chair", "A lovely chair!"], conn)
         curd.add_data(TestProduct.TABLE_NAME, TestProduct.PRIMARY_KEY, TestProduct.ATTRIBUTES, ["Magdal", 9.55, "Table", "A lovely table!"], conn)
@@ -32,27 +32,27 @@ class TestProduct():
         curd.add_data(TestProduct.TABLE_NAME, TestProduct.PRIMARY_KEY, TestProduct.ATTRIBUTES, ["NICETUN", 3.55, "Wall Cover", "A lovely cover!"], conn)
     
     def test_sort_by_price_monotonicity(self):
-        conn = self.pre_setup()
+        conn = pre_setup(database_schemas.create_products, self.add_products_to_table)
         price_df = curd.return_df_of_table(TestProduct.TABLE_NAME, conn)
         assert(not price_df.price.is_monotonic_increasing)
         post_setup(conn)
 
     # Sorts items by price
     def test_sort_by_price(self):
-        conn = self.pre_setup()
+        conn = pre_setup(database_schemas.create_products, self.add_products_to_table)
         price_df = search.sort_by_price(conn)
         assert(price_df.price.is_monotonic_increasing)
         post_setup(conn)
 
     def test_sort_by_category_monotonicity(self):
-        conn = self.pre_setup()
+        conn = pre_setup(database_schemas.create_products, self.add_products_to_table)
         price_df = curd.return_df_of_table(TestProduct.TABLE_NAME, conn)
         assert(not price_df.category.is_monotonic_increasing)
         post_setup(conn)
 
     # Sorts/queries items by category
     def test_sort_by_category(self):
-        conn = self.pre_setup()
+        conn = pre_setup(database_schemas.create_products, self.add_products_to_table)
         price_df = search.sort_by_category(conn)
         assert(price_df.category.is_monotonic_increasing)
         post_setup(conn)
@@ -63,12 +63,6 @@ class TestReview():
     PRIMARY_KEY = "review_id"
     ATTRIBUTES = ["user_id", "product_id", "review"]
     
-    def pre_setup(self):
-        conn = sqlite3.connect(DATABASE_NAME)
-        conn.execute(database_schemas.create_reviews)
-        self.add_products_to_table(conn)
-        return conn
-    
     def add_products_to_table(self, conn):
         curd.add_data(TestReview.TABLE_NAME, TestReview.PRIMARY_KEY, TestReview.ATTRIBUTES, [1, 1, 5], conn)
         curd.add_data(TestReview.TABLE_NAME, TestReview.PRIMARY_KEY, TestReview.ATTRIBUTES, [1, 2, 3], conn)
@@ -76,14 +70,14 @@ class TestReview():
         curd.add_data(TestReview.TABLE_NAME, TestReview.PRIMARY_KEY, TestReview.ATTRIBUTES, [1, 4, 1], conn)
     
     def test_sort_by_popularity(self):
-        conn = self.pre_setup()
+        conn = pre_setup(database_schemas.create_reviews, self.add_products_to_table)
         reviews_df = curd.return_df_of_table(TestReview.TABLE_NAME, conn)
         assert(not reviews_df.review.is_monotonic_decreasing)
         post_setup(conn)
     
     # Sorts items by popularity (highest to lowest)
     def test_sort_by_popularity(self):
-        conn = self.pre_setup()
+        conn = pre_setup(database_schemas.create_reviews, self.add_products_to_table)
         reviews_df = search.sort_by_popularity(conn)
         assert(reviews_df.review.is_monotonic_decreasing)
         post_setup(conn)
