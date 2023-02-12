ed6fe29032c4539461aa619825e639aeaaffab33
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-02-12 13:34:45 -0800

    Shortened class variable names, added login route tests

--- tests/server/test_ikeaAPI.py
+++ tests/server/test_ikeaAPI.py
@@ -1,81 +1,135 @@
 class TestRegisterRoute():
-    REGISTER_EMAIL = "testingtesting123@gmail.com"
-    REGISTER_EMAIL_DICT = {"email": REGISTER_EMAIL}
+    EMAIL = "testingtesting123@gmail.com"
+    NAME = "Mustafa"
+    PASSWORD = "y0u_c4n't_gU3$$_th1s"
+    EMAIL_DICT = {"email": EMAIL}
     
-    REGISTER_DICT = {
-            "name": "Mustafa",
-            "password": "y0u_c4n't_gU3$$_th1s",
-            "email": REGISTER_EMAIL,
+    DICT = {
+            "name": NAME,
+            "password": PASSWORD,
+            "email": EMAIL,
             "address": "5555 NW Testing St\nPytest, WY 93321",
             "payment_method": "Cashier's Check"
         }
     
-    REGISTER_CURRENT_EMAIL_DICT = {
-            "name": "Mustafa",
-            "password": "y0u_c4n't_gU3$$_th1s",
-            "current_email": REGISTER_EMAIL,
-            "email": REGISTER_EMAIL,
+    CURRENT_EMAIL_DICT = {
+            "name": NAME,
+            "password": PASSWORD,
+            "current_email": EMAIL,
+            "email": EMAIL,
             "address": "5555 NW Testing St\nPytest, WY 93321",
             "payment_method": "Cashier's Check"
         }
 
-    REGISTER_URL = '/register/'
+    URL = '/register/'
     
     def register_post(self, client):
-        client.post(self.REGISTER_URL, json=self.REGISTER_DICT)
+        client.post(self.URL, json=self.DICT)
     
     ### POST
     
     def test_register_post_type(self, client):
-        response = client.post(self.REGISTER_URL, json=self.REGISTER_DICT)
+        response = client.post(self.URL, json=self.DICT)
         assert(response.content_type == 'text/html; charset=utf-8')   
         
     def test_register_post_data(self, client):
-        response = client.post(self.REGISTER_URL, json=self.REGISTER_DICT)
+        response = client.post(self.URL, json=self.DICT)
         assert(response.get_data().decode('UTF-8') == 'account added')
         
     def test_register_post_exists(self, client):
-        response = client.post(self.REGISTER_URL, json=self.REGISTER_DICT)
-        response = client.post(self.REGISTER_URL, json=self.REGISTER_DICT)
+        response = client.post(self.URL, json=self.DICT)
+        response = client.post(self.URL, json=self.DICT)
         assert(response.get_data().decode('UTF-8') == 'email already exists')  
     
     ### PUT
     
     def test_register_put_type(self, client):
-        response = client.put(self.REGISTER_URL, json=self.REGISTER_CURRENT_EMAIL_DICT)
+        response = client.put(self.URL, json=self.CURRENT_EMAIL_DICT)
         assert(response.content_type == 'text/html; charset=utf-8')
         
     def test_register_put_data(self, client):
         self.register_post(client)
-        response = client.put(self.REGISTER_URL, json=self.REGISTER_CURRENT_EMAIL_DICT)
+        response = client.put(self.URL, json=self.CURRENT_EMAIL_DICT)
         assert(response.get_data().decode('UTF-8') == 'account updated')
         
     def test_register_put_invalid(self, client):
-        response = client.put(self.REGISTER_URL, json=self.REGISTER_CURRENT_EMAIL_DICT)
+        response = client.put(self.URL, json=self.CURRENT_EMAIL_DICT)
         assert(response.get_data().decode('UTF-8') == 'invalid email')  
     
     ### DELETE
 
     def test_register_delete_type(self, client):
         self.register_post(client)
-        response = client.delete(self.REGISTER_URL, json=self.REGISTER_EMAIL_DICT)
+        response = client.delete(self.URL, json=self.EMAIL_DICT)
         assert(response.content_type == 'text/html; charset=utf-8')
         
     def test_register_delete_data(self, client):
         self.register_post(client)
-        response = client.delete(self.REGISTER_URL, json=self.REGISTER_EMAIL_DICT)
+        response = client.delete(self.URL, json=self.EMAIL_DICT)
         assert(response.get_data().decode('UTF-8') == 'account deleted')
     
     def test_register_delete_invalid(self, client):
-        response = client.delete(self.REGISTER_URL, json=self.REGISTER_EMAIL_DICT)
+        response = client.delete(self.URL, json=self.EMAIL_DICT)
         assert(response.get_data().decode('UTF-8') == 'invalid email')
         
     ### GET
     
     def test_register_get_type(self, client):
-        response = client.get(self.REGISTER_URL)
+        response = client.get(self.URL)
         assert(response.content_type == 'text/html; charset=utf-8')
         
     def test_register_get_data(self, client):
-        response = client.get(self.REGISTER_URL)
+        response = client.get(self.URL)
         assert(response.get_data().decode('UTF-8') == 'nothing here')
+        
+class TestLoginRoute():
+    DICT = {
+            "email": TestRegisterRoute.EMAIL,
+            "password": TestRegisterRoute.PASSWORD,
+        }
+    
+    URL = '/login/'
+    
+    ### POST
+    
+    def test_login_post_type(self, client):
+        response = client.post(self.URL, json=self.DICT)
+        assert(response.content_type == 'text/html; charset=utf-8')   
+        
+    def test_login_post_data(self, client):
+        TestRegisterRoute.register_post(TestRegisterRoute, client)
+        response = client.post(self.URL, json=self.DICT)
+        assert(response.get_data().decode('UTF-8') == f'Welcome {TestRegisterRoute.NAME}')
+        
+    def test_login_post_logged_in(self, client):
+        TestRegisterRoute.register_post(TestRegisterRoute, client)
+        client.post(self.URL, json=self.DICT)
+        response = client.post(self.URL, json=self.DICT)
+        assert(response.get_data().decode('UTF-8') == 'Already Logged In')   
+        
+    def test_login_post_invalid(self, client):
+        response = client.post(self.URL, json=self.DICT)
+        assert(response.get_data().decode('UTF-8') == 'invalid')
+        
+# class TestLogoutoute():
+#     DICT = {
+#             "email": TestRegisterRoute.EMAIL,
+#             "password": TestRegisterRoute.PASSWORD,
+#         }
+    
+#     URL = '/login/'
+    
+#     ### POST
+    
+#     def test_login_post_type(self, client):
+#         response = client.post(self.URL, json=self.DICT)
+#         assert(response.content_type == 'text/html; charset=utf-8')   
+        
+#     def test_login_post_data(self, client):
+#         TestRegisterRoute.register_post(TestRegisterRoute, client)
+#         response = client.post(self.URL, json=self.DICT)
+#         assert(response.get_data().decode('UTF-8') == f'Welcome {TestRegisterRoute.NAME}')
+        
+#     def test_login_post_invalid(self, client):
+#         response = client.post(self.URL, json=self.DICT)
+#         assert(response.get_data().decode('UTF-8') == 'invalid')  
