0ae79e2a6b6836c5a884d24a075ce801b0809226
Mustafa Radheyyan <mustafa.radheyyan@gmail.com>
2023-02-12 16:08:29 -0800

    Refactored productAPI ProductIDRoute test methods

--- tests/server/test_productAPI.py
+++ tests/server/test_productAPI.py
@@ -40,15 +40,16 @@ class TestProductIdRoute():
     
     ### GET
     
-    def test_product_id_get_type(self, client):
+    def initialize_database_login_and_get_url(self, client):
         TestProductsRoute().initialize_products_database_and_login(client)
-        response = client.get(self.URL)
+        return client.get(self.URL)
+    
+    def test_product_id_get_type(self, client):
+        response = self.initialize_database_login_and_get_url(client)
         assert(response.content_type == 'text/html; charset=utf-8')
         
     def test_product_id_get_data(self, client):
-        TestProductsRoute().initialize_products_database_and_login(client)
-        response = client.get(self.URL)
-        # print(response.get_data())
+        response = self.initialize_database_login_and_get_url(client)
         assert(all([x in response.get_data().decode('UTF-8') for x in OUTDOOR_FURNITURE_LIST_MATCH]))
         
     def test_products_id_get_not_logged_in(self, client):
